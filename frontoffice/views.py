2d48670d0ab02e5cb5f9b2808dbe8162b75e8f7f
MrAbdelaziz <elouahab.abdelaziz@gmail.com>
2020-05-13 12:14:05 +0000

    update : login page -> err message

--- frontoffice/views.py
+++ frontoffice/views.py
@@ -5,8 +5,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import *
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.contrib import messages
 def login(requset):
-    return render(requset, "login.html")
+    return render(requset, "login.html",)
 class LoginView(TemplateView):
 
   template_name = 'login.html'
@@ -19,7 +20,7 @@ class LoginView(TemplateView):
     if user is not None and user.is_active:
         login(request)
         return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL )
-
+    messages.success(request, 'mot de passe ou nom d''utilisateur incorrect')
     return render(request, self.template_name)
 
 
